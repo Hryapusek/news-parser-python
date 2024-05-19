caabf8c1a266f24af133a723ff11d5dd549cc7a0
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-19 10:03:25 +0300

    text noramalizizng added

--- result/textprocess/text_processing.py
+++ result/textprocess/text_processing.py
@@ -1,5 +1,21 @@
+import nltk
+import string
+nltk.download("stopwords")
+
+from nltk.corpus import stopwords
+from pymystem3 import Mystem
+from string import punctuation
+
+mystem = Mystem() 
+russian_stopwords = stopwords.words("russian")
 
 class TextProcessor:
     @staticmethod
-    def normalize(text: str):
-        pass
+    def normalize(text: str) -> list[str]:
+        tokens = mystem.lemmatize(text.lower())
+        tokens = [token for token in tokens if token not in russian_stopwords
+                and not string.ascii_letters in token
+                and token != " " 
+                and token.strip() not in punctuation]
+        return tokens
+
