fa72b64acf9bcd873dc2561452c344415d867036
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-19 14:17:45 +0300

    tests changes

--- src/articleloader/test/test_article_loader.py
+++ src/articleloader/test/test_article_loader.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 import unittest
 
@@ -9,4 +10,14 @@ class TestArticleLoader(unittest.TestCase):
 
     def test_default(self):
         ArticleLoader.load_articles_from_internet(datetime(2024, 5, 13), datetime(2024, 5, 13))
+        asyncio.run(asyncio.sleep(0.1))
+        while not ArticleLoader.is_finished():
+            print(*ArticleLoader.get_status())
+            asyncio.run(asyncio.sleep(0.1))
+        print(*ArticleLoader.get_status())
+
+    def test_stop(self):
+        ArticleLoader.load_articles_from_internet(datetime(2024, 5, 13), datetime(2024, 5, 13))
+        asyncio.run(asyncio.sleep(20))
+        ArticleLoader.call_stop()
         ArticleLoader.wait_for_finish()
