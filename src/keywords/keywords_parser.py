d5fca74b4a2c539aff2b4d08abbc958eb0ea3761
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-20 12:11:11 +0300

    fix: keywords parser now works without re module

--- src/keywords/keywords_parser.py
+++ src/keywords/keywords_parser.py
@@ -15,39 +15,29 @@ class Category:
 class KeywordsParser:
 
     @staticmethod
-    def parse_file(file_path):
-        categories = []
+    def parse_file(file_path) -> list[Category]:
+        categories: list[Category] = []
 
         with open(file_path, 'r', encoding='utf-8') as file:
             text = file.read()
 
-            # Define regular expressions to match sections and subsections
-            section_pattern = r'#\[(.*?)\]'
-            subsection_pattern = r'##\[(.*?)\]'
+            lines = text.strip().split('\n')
 
-            # Find all matches for sections
-            sections = re.findall(section_pattern, text, re.DOTALL)
-            
-            # Iterate over sections
-            for section_name in sections:
-                section = Category()
-                section.name = section_name
-
-                # Find all matches for subsections within this section
-                subsections_text = re.split(section_pattern, text)[sections.index(section_name) + 1]
-                subsections = re.findall(subsection_pattern, subsections_text, re.DOTALL)
-
-                # Iterate over subsections
-                for subsection_name in subsections:
-                    subsection = SubCategory()
-                    section.subcategories.append(subsection)
-                    subsection.name = subsection_name
-
-                    # Find all contents within this subsection
-                    subsection_text = re.split(subsection_pattern, subsections_text)[subsections.index(subsection_name) + 1].strip()
-                    subsection.keywords = TextProcessor.normalize(subsection_text)
-
-                # Add the section to the list of classes
-                categories.append(section)
-
+            for line in lines:
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+                if line.startswith('#['):
+                    categories.append(Category())
+                    categories[-1].name = line[2:-1]
+                elif line.startswith('##['):
+                    categories[-1].subcategories.append(SubCategory())
+                    categories[-1].subcategories[-1].name = line[2:-1]
+                else:
+                    try:
+                        categories[-1].subcategories[-1].keywords.extend(TextProcessor.normalize(line))
+                    except IndexError:
+                        print(f"Text after cathegory is not allowed! Check category {categories[-1].name}")
+                        raise
+        
         return categories
